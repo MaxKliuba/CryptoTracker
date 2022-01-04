d9d87ded3d84a961efa08f79b318f85c74f570fc
MaxKliuba <klyubamaks@gmail.com>
2022-01-04 13:53:11 +0200

    add chart example

--- app/src/main/java/com/maxclub/android/cryptotracker/CryptoTrackerFragment.java
+++ app/src/main/java/com/maxclub/android/cryptotracker/CryptoTrackerFragment.java
@@ -1,5 +1,6 @@
 package com.maxclub.android.cryptotracker;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +12,25 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class CryptoTrackerFragment extends Fragment {
 
     private ActionBar mActionBar;
+    private LineChart mLineChart;
 
     public static CryptoTrackerFragment newInstance() {
         return new CryptoTrackerFragment();
@@ -38,6 +51,112 @@ public class CryptoTrackerFragment extends Fragment {
         mActionBar.setTitle(getString(R.string.btc_usd_title));
         mActionBar.setSubtitle(String.format(Locale.getDefault(), "%.2f $", 47128.145));
 
+        mLineChart = view.findViewById(R.id.chart);
+        mLineChart.setViewPortOffsets(0, 0, 0, 0);
+        mLineChart.setBackgroundColor(getResources().getColor(R.color.color_surface));
+
+        // no description text
+        mLineChart.getDescription().setEnabled(false);
+
+        // enable touch gestures
+        mLineChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mLineChart.setDragEnabled(true);
+        mLineChart.setScaleEnabled(true);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mLineChart.setPinchZoom(true);
+
+        mLineChart.setDrawGridBackground(false);
+        mLineChart.setMaxHighlightDistance(300);
+
+        XAxis xAxis = mLineChart.getXAxis();
+        //x.setEnabled(false);
+        xAxis.setLabelCount(6, false);
+        xAxis.setTextColor(getResources().getColor(R.color.color_on_surface));
+        xAxis.setTextSize(16.0f);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisLineColor(getResources().getColor(R.color.color_surface));
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setGranularity(1f);
+
+        YAxis yAxis = mLineChart.getAxisLeft();
+        //y.setTypeface(tfLight);
+        yAxis.setLabelCount(6, false);
+        yAxis.setTextColor(getResources().getColor(R.color.color_on_surface));
+        yAxis.setTextSize(16.0f);
+        yAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+        yAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+        yAxis.setAxisLineColor(getResources().getColor(R.color.color_surface));
+        yAxis.setYOffset(-9f);
+
+        mLineChart.getAxisRight().setEnabled(false);
+
+        mLineChart.getLegend().setEnabled(false);
+
+        //mLineChart.animateXY(2000, 2000);
+
+        setData(20, 10);
+
+        // don't forget to refresh the drawing
+        mLineChart.invalidate();
+
         return view;
     }
+
+    private void setData(int count, float range) {
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * (range + 1)) + 20;
+            values.add(new Entry(i, val));
+        }
+
+        LineDataSet set1;
+
+        if (mLineChart.getData() != null &&
+                mLineChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) mLineChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            mLineChart.getData().notifyDataChanged();
+            mLineChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet 1");
+
+            set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            set1.setCubicIntensity(0.1f);
+            set1.setDrawFilled(true);
+            set1.setDrawCircles(false);
+            set1.setLineWidth(1.8f);
+            set1.setCircleRadius(4f);
+            set1.setCircleColor(getResources().getColor(R.color.color_on_surface));
+            set1.setHighLightColor(getResources().getColor(R.color.color_on_surface));
+            set1.setColor(getResources().getColor(R.color.teal_700));
+            set1.setFillColor(getResources().getColor(R.color.teal_700));
+            set1.setFillAlpha(50);
+            set1.setDrawHorizontalHighlightIndicator(true);
+            set1.setFillFormatter(new IFillFormatter() {
+                @Override
+                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                    return mLineChart.getAxisLeft().getAxisMinimum();
+                }
+            });
+
+            // create a data object with the data sets
+            LineData data = new LineData(set1);
+            //data.setValueTypeface(tfLight);
+            data.setValueTextSize(9f);
+            data.setDrawValues(false);
+
+            // set data
+            mLineChart.setData(data);
+        }
+    }
+
 }
